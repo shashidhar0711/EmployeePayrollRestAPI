b13c2eb4b0095a477a3fe65f9f794b501cc54719
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-01 21:09:01 +0530

    [sasic] Add . Test Cases For Update The Employee Salary in JsonServer

--- EmployeePayrollRestApiTest/EmployeePayrollRestApiTest.cs
+++ EmployeePayrollRestApiTest/EmployeePayrollRestApiTest.cs
@@ -79,6 +79,7 @@ namespace EmployeePayrollRestApi
         [TestMethod]
         public void GivenMultipleEmployee_OnPost_ShouldReturnEmployees()
         {
+            /// Arrange
             List<EmployeePayrollRestApi> employeePayrollListRestApi = new List<EmployeePayrollRestApi>();
             employeePayrollListRestApi.Add(new EmployeePayrollRestApi { Name = "Varsha", Salary = "29000" });
             employeePayrollListRestApi.Add(new EmployeePayrollRestApi { Name = "Sushma", Salary = "31000" });
@@ -103,6 +104,29 @@ namespace EmployeePayrollRestApi
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<EmployeePayrollRestApi> dataResponse = JsonConvert.DeserializeObject<List<EmployeePayrollRestApi>>(response.Content);
             Assert.AreEqual(6, dataResponse.Count);
+        }
+
+        /// <summary>
+        /// UC4
+        /// Givens the employee on update should return updated employee.
+        /// </summary>
+        [TestMethod]
+        public void GivenEmployee_OnUpdate_ShouldReturnUpdatedEmployee()
+        {
+            /// Arrange
+            RestRequest request = new RestRequest("/employee/1", Method.PUT);
+            JObject jObjectBody = new JObject();
+            jObjectBody.Add("name", "Shashi");
+            jObjectBody.Add("salary", "27800");
+            request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+            /// Act
+            IRestResponse response = client.Execute(request);
+            /// Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            EmployeePayrollRestApi dataResponse = JsonConvert.DeserializeObject<EmployeePayrollRestApi>(response.Content);
+            Assert.AreEqual("Shashi", dataResponse.Name);
+            Assert.AreEqual("27800", dataResponse.Salary);
+            Console.WriteLine(response.Content);
         }
     }
 }
