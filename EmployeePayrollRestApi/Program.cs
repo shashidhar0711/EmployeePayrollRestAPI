b64d4418fe1db5df53a2c8db7c3088cbe87595af
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-01 19:53:08 +0530

    Add project files.

---
+++ EmployeePayrollRestApi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeePayrollRestApi
+{
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Rest Api");
+        }
+    }
+}
