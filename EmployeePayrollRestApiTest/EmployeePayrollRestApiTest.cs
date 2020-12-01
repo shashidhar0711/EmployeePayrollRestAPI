2ada020df0e4234c73e80df2ec6c70c6b5327bca
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-01 20:58:24 +0530

    [sasic] Add . Test Cases For Adding Multiple Employees InTo JsonServer

--- EmployeePayrollRestApiTest/EmployeePayrollRestApiTest.cs
+++ EmployeePayrollRestApiTest/EmployeePayrollRestApiTest.cs
@@ -71,5 +71,38 @@ namespace EmployeePayrollRestApi
             Assert.AreEqual("27000", dataResponse.Salary);
             Console.WriteLine(response.Content);
         }
+
+        /// <summary>
+        /// UC3
+        /// Givens the multiple employee on post should return employees.
+        /// </summary>
+        [TestMethod]
+        public void GivenMultipleEmployee_OnPost_ShouldReturnEmployees()
+        {
+            List<EmployeePayrollRestApi> employeePayrollListRestApi = new List<EmployeePayrollRestApi>();
+            employeePayrollListRestApi.Add(new EmployeePayrollRestApi { Name = "Varsha", Salary = "29000" });
+            employeePayrollListRestApi.Add(new EmployeePayrollRestApi { Name = "Sushma", Salary = "31000" });
+
+            employeePayrollListRestApi.ForEach(employeeData =>
+            {
+                RestRequest request = new RestRequest("/employee", Method.POST);
+                JObject jObjectBody = new JObject();
+                jObjectBody.Add("Name", employeeData.Name);
+                jObjectBody.Add("Salary", employeeData.Salary);
+                /// Act
+                request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                /// Assert
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                EmployeePayrollRestApi dataResponse = JsonConvert.DeserializeObject<EmployeePayrollRestApi>(response.Content);
+                Assert.AreEqual(employeeData.Name, dataResponse.Name);
+                Assert.AreEqual(employeeData.Salary, dataResponse.Salary);
+                Console.WriteLine(response.Content);
+            });
+            IRestResponse response = GetEmployeeList();
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<EmployeePayrollRestApi> dataResponse = JsonConvert.DeserializeObject<List<EmployeePayrollRestApi>>(response.Content);
+            Assert.AreEqual(6, dataResponse.Count);
+        }
     }
 }
